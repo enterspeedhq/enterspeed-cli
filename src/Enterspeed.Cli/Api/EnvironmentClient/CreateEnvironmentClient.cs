b5fbf92cfd84e13dc2cddd9aff4d6ba23c64baa1
Jesper Thind <jet@enterspeed.com>
2022-11-10 10:12:53 +0100

    EnvironmentClient API

---
+++ src/Enterspeed.Cli/Api/EnvironmentClient/CreateEnvironmentClient.cs
@@ -0,0 +1,48 @@
+﻿using Enterspeed.Cli.Domain.Models;
+using Enterspeed.Cli.Services.EnterspeedClient;
+using MediatR;
+using RestSharp;
+using System.Text.Json.Serialization;
+
+namespace Enterspeed.Cli.Api.EnvironmentClient
+{
+    public class CreateEnvironmentClientRequest : IRequest<CreateEnvironmentClientResponse>
+    {
+        public string Name { get; set; }
+        public string[] AllowedDomains { get; set; }
+
+        public bool RegenerateAccessKey { get; set; }
+
+        [JsonIgnore]
+        public EnvironmentId EnvironmentId { get; }
+
+        public CreateEnvironmentClientRequest(EnvironmentId environmentId)
+        {
+            EnvironmentId = environmentId;
+        }
+    }
+
+    public class CreateEnvironmentClientResponse
+    {
+        public EnvironmentClientId EnvironmentClientId { get; set; }
+    }
+
+    public class CreateEnvironmentClientRequestRequestHandler : IRequestHandler<CreateEnvironmentClientRequest, CreateEnvironmentClientResponse>
+    {
+        private readonly IEnterspeedClient _enterspeedClient;
+
+        public CreateEnvironmentClientRequestRequestHandler(IEnterspeedClient enterspeedClient)
+        {
+            _enterspeedClient = enterspeedClient;
+        }
+
+        public async Task<CreateEnvironmentClientResponse> Handle(CreateEnvironmentClientRequest createEnvironmentClientRequest, CancellationToken cancellationToken)
+        {
+            var request = new RestRequest($"tenant/environment-clients/{createEnvironmentClientRequest.EnvironmentId.EnvironmentGuid}", Method.Post)
+                .AddJsonBody(createEnvironmentClientRequest);
+
+            var response = await _enterspeedClient.ExecuteAsync<CreateEnvironmentClientResponse>(request, cancellationToken);
+            return response;
+        }
+    }
+}
---
+++ src/Enterspeed.Cli/Api/EnvironmentClient/UpdateEnvironmentClient.cs
@@ -0,0 +1,48 @@
+﻿using Enterspeed.Cli.Domain.Models;
+using Enterspeed.Cli.Services.EnterspeedClient;
+using MediatR;
+using RestSharp;
+using System.Text.Json.Serialization;
+
+namespace Enterspeed.Cli.Api.EnvironmentClient
+{
+    public class UpdateEnvironmentClientRequest : IRequest<UpdateEnvironmentClientResponse>
+    {
+        public string Name { get; set; }
+        public string[] AllowedDomains { get; set; }
+
+        public bool RegenerateAccessKey { get; set; }
+
+        [JsonIgnore]
+        public EnvironmentClientId EnvironmentClientId { get; }
+
+        public UpdateEnvironmentClientRequest(EnvironmentClientId environmentClientId)
+        {
+            EnvironmentClientId = environmentClientId;
+        }
+    }
+
+    public class UpdateEnvironmentClientResponse
+    {
+        public EnvironmentClientId EnvironmentClientId { get; set; }
+    }
+
+    public class UpdateEnvironmentClientRequestRequestHandler : IRequestHandler<UpdateEnvironmentClientRequest, UpdateEnvironmentClientResponse>
+    {
+        private readonly IEnterspeedClient _enterspeedClient;
+
+        public UpdateEnvironmentClientRequestRequestHandler(IEnterspeedClient enterspeedClient)
+        {
+            _enterspeedClient = enterspeedClient;
+        }
+
+        public async Task<UpdateEnvironmentClientResponse> Handle(UpdateEnvironmentClientRequest updateEnvironmentClientRequest, CancellationToken cancellationToken)
+        {
+            var request = new RestRequest($"tenant/environment-clients/{updateEnvironmentClientRequest.EnvironmentClientId.EnvironmentGuid}/{updateEnvironmentClientRequest.EnvironmentClientId.ClientGuid}/", Method.Put)
+                .AddJsonBody(updateEnvironmentClientRequest);
+
+            var response = await _enterspeedClient.ExecuteAsync<UpdateEnvironmentClientResponse>(request, cancellationToken);
+            return response;
+        }
+    }
+}
