e9de6237e144586e2cf75562931c54496735712b
MHove89 <mathiashove02@gmail.com>
2022-11-15 13:58:59 -0600

    Added missing schema properties to schema class

--- src/Enterspeed.Cli/Services/FileService/Models/SchemaBaseProperties.cs
+++ src/Enterspeed.Cli/Services/FileService/Models/SchemaBaseProperties.cs
@@ -1,15 +1,23 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace Enterspeed.Cli.Services.FileService.Models;
-
-public class SchemaBaseProperties
+namespace Enterspeed.Cli.Services.FileService.Models
 {
-    [JsonPropertyName("triggers")]
-    public object Triggers { get; set; }
 
-    [JsonPropertyName("route")]
-    public object Route { get; set; }
+    public class SchemaBaseProperties
+    {
+        [JsonPropertyName("triggers")]
+        public object Triggers { get; set; }
 
-    [JsonPropertyName("properties")]
-    public object Properties { get; set; }
+        [JsonPropertyName("environments")]
+        public object[] Environments { get; set; }
+
+        [JsonPropertyName("actions")]
+        public object[] Actions { get; set; }
+
+        [JsonPropertyName("route")]
+        public object Route { get; set; }
+
+        [JsonPropertyName("properties")]
+        public object Properties { get; set; }
+    }
 }
