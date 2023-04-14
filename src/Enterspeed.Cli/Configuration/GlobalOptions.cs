96a003d163305a7a56bb220edeffa0860c1126bf
Jesper Thind <66388377+jthind@users.noreply.github.com>
2023-04-14 14:08:24 +0200

    Added custom endpoint option (#16)

--- src/Enterspeed.Cli/Configuration/GlobalOptions.cs
+++ src/Enterspeed.Cli/Configuration/GlobalOptions.cs
@@ -1,3 +1,4 @@
+using Enterspeed.Cli.Domain.Exceptions;
 using Enterspeed.Cli.Services.ConsoleOutput;
 
 namespace Enterspeed.Cli.Configuration
@@ -6,12 +7,33 @@ namespace Enterspeed.Cli.Configuration
     {
         public string ApiKeyValue { get; private set; }
         public OutputStyle OutPutStyle { get; private set; }
+        public Uri CustomEndpoint { get; private set; }
 
-        public void Set(string apiKeyValue, OutputStyle outPutStyle)
+        public void Set(string apiKeyValue, OutputStyle outPutStyle, string customEndpoint)
         {
             if (!string.IsNullOrEmpty(apiKeyValue))
             {
                 ApiKeyValue = apiKeyValue;
+            }
+
+            if (!string.IsNullOrEmpty(customEndpoint))
+            {
+                if (ApiKeyValue == null)
+                {
+                    throw new Exception("--customEndpoint option can only be used when --apiKey is set");
+                }
+
+                var uri = new Uri(customEndpoint);
+
+                if (uri.Scheme is "http" or "https")
+                {
+                    CustomEndpoint = uri;
+                }
+                else
+                {
+                    throw new UriFormatException("Url scheme must be http or https");
+                }
+               
             }
 
             OutPutStyle = outPutStyle;
